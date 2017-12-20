6d48053911367445064d042304865d8388813dba
Supun Dissanayake <supunlakshandissanayake@gmail.com>
2017-12-20 23:44:06 +0530

    added local mongo db

--- db/database.js
+++ db/database.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 //connect to databse
-mongoose.connect('mongodb://user:password@ds141796.mlab.com:41796/todo-db', {
-    useMongoClient: true
+mongoose.connect('mongodb://localhost/myapp', {
+  useMongoClient: true
 });
 
 //create a schema
 var todoSchema = new mongoose.Schema({
-    task: String
+  task: String
 });
 
 module.exports.ToDo = mongoose.model('Todo', todoSchema);
